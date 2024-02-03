9b150a4f1f85fad80a9b7592a5e42cf6e0e99241
Diogo Goncalves <diogogoncalves91@icloud.com>
2024-02-03 22:55:27 +0000

    first commit

---
+++ src/main/java/com/logon/gest4all/Gest4allApplication.java
@@ -0,0 +1,13 @@
+package com.logon.gest4all;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gest4allApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gest4allApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logon/gest4all/Gest4allApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logon.gest4all;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gest4allApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
